b6fcb851687717791efe14eced1ed5aa9dc018d2
kms <min9.9999u@gmail.com>
2021-05-12 18:27:40 +0900

    update

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
   $(".slider").bxSlider({
     pager:false,
   });
+
+  $(".toggleMenu").click(function(){
+    const toggle = $(".toggleMenu").attr("class");
+    if(toggle == "toggleMenu"){
+    $(".toggleMenu").addClass("change");
+    $("#main_menu").stop().animate({left:0});
+
+  }else{
+    $(".toggleMenu").removeClass("change");
+    $("#main_menu").stop().animate({left:"-100%"});
+  }
+  });
+
+  $(window).resize(function(){
+    $("#main_menu").removeAttr("style");
+    $(".toggleMenu").removeClass("change");
+  });
+
 });
